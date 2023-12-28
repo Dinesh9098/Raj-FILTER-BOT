771c1d1794558b7c66b3433494bc6a14c586a65c
Dinesh9098 <151295073+Dinesh9098@users.noreply.github.com>
2023-12-28 22:52:26 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,16 +1,14 @@
-# Don't Remove Credit @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot @Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+
 
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Dinesh9098/VJ-FILTER-BOT.git /VJ-FILTER-BOT 
+  git clone https://github.com/Dinesh9098/Raj-FILTER-BOT.git /VJ-FILTER-BOT 
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /VJ-FILTER-BOT 
+  git clone $UPSTREAM_REPO /Raj-FILTER-BOT
 fi
-cd /VJ-FILTER-BOT 
+cd /Raj-FILTER-BOT
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
